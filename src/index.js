bd767f3d24414b8a7660849eca2a246c00c212e4
Tom Martin <tom@Poe.local>
2019-05-29 06:22:12 -0700

    Adds switch and updates routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
@@ -11,16 +11,20 @@ import Signup from './components/auth/Signup';
 import Signout from './components/auth/Signout';
 import Characters from './components/characters/Characters';
 import CharacterCreate from './components/characters/CharacterCreate';
+import CharacterEdit from './components/characters/CharacterEdit';
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
             <App>
-                <Route path='/signup' component={Signup} />
-                <Route path='/signin' component={Signin} />
-                <Route path='/signout' component={Signout} />
-                <Route path='/characters' component={Characters} />
-                <Route path='/characters/new' component={CharacterCreate} />
+                <Switch>
+                    <Route path='/signup' component={Signup} />
+                    <Route path='/signin' component={Signin} />
+                    <Route path='/signout' component={Signout} />
+                    <Route path='/characters' exact component={Characters} />
+                    <Route path='/characters/new' component={CharacterCreate} />
+                    <Route path='/characters/:characterId/edit' component={CharacterEdit} />
+                </Switch>
             </App>
         </BrowserRouter>
     </Provider>,
