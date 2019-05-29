97d7b3e1629cb3649225aef9d69e47c0adeb8717
Tom Martin <tom@Poe.local>
2019-05-29 07:03:28 -0700

    Adds character component

---
+++ src/components/characters/Character.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchCharacter } from '../../redux/actions/characterActions';
+
+class Character extends Component {
+    componentDidMount() {
+        this.props.fetchCharacter(this.props.match.params.characterId);
+    }
+
+    render() {
+        return (
+            <div>{this.props.character.name}</div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        character: state.characters[ownProps.match.params.characterId],
+        races: state.characterOptions.races,
+        alignments: state.characterOptions.alignments,
+        classes: state.characterOptions.classes
+    };
+};
+
+export default connect(mapStateToProps, { fetchCharacter })(Character);
