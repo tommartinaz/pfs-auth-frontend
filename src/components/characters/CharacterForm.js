e559ef38cea1af92c0e79d9ca1e6bc3839025802
Tom Martin <tom@Poe.local>
2019-06-02 05:36:07 -0700

    updates styling of character components

--- src/components/characters/CharacterForm.js
+++ src/components/characters/CharacterForm.js
@@ -2,34 +2,51 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { reduxForm, Field } from 'redux-form';
-
+import { 
+    Button,
+    Form,
+    Grid,
+    Icon,
+} from 'semantic-ui-react';
 
 class CharacterForm extends Component {
 
     renderInput = ({ input, label }) => {
         return (
-            <div className="field">
-                <label>{label}</label>
-                <input { ...input } autoComplete="off" />
-            </div>
+            <Form.Input
+                placeholder={label}
+                {...input}
+                label={label}
+            />
         );
     }
 
     renderSelector = ({ input, label, selector }) => {
         const list = selector.map(el => {
             return (
-                <option key={el.id} value={el.id}>{el.name.toUpperCase()}</option>
+                { key: el.id, text: el.name, value: el.id }
             );
         });
         return (
-            <div className="field">
-                <label>{label}</label>
-                <select {...input}>
-                    <option value='' />
-                    {list}
-                </select>
-            </div>
+            <Form.Select
+                label={label}
+                name={input.name}
+                onChange={(e, { value }) => input.onChange(value)}
+                options={list}
+                value={input.value}
+            />
         );
+    }
+
+    renderCheckbox = ({ input, label, name}) => {
+        return (
+            <Form.Checkbox
+                checked={!!input.value}
+                name={name}
+                label={label}
+                onChange={(e, { checked }) => input.onChange(checked)}
+            />
+        )
     }
 
     onSubmit = formValues => {
@@ -39,48 +56,57 @@ class CharacterForm extends Component {
     render() {
         const { handleSubmit, pristine, reset, alignments, races, classes } = this.props;
         return (
-            <form className="ui form" onSubmit={handleSubmit(this.onSubmit)}>
-                <div className="fields">
-                    <div className="six wide field">
-                        <Field name="name" component={this.renderInput} label="Character Name" />
-                    </div>
-                    <div className="six wide field">
-                        <div className="fields">
-                            <div className="three wide field">
-                                <Field name="str" component={this.renderInput} label="STR" />
-                            </div>
-                            <div className="three wide field">
-                                <Field name="dex" component={this.renderInput} label="DEX" />
-                            </div>
-                            <div className="three wide field">
-                                <Field name="con" component={this.renderInput} label="CON" />
-                            </div>
-                            <div className="three wide field">
-                                <Field name="int" component={this.renderInput} label="INT" />
-                            </div>
-                            <div className="three wide field">
-                                <Field name="wis" component={this.renderInput} label="WIS" />
-                            </div>
-                            <div className="three wide field">
-                                <Field name="cha" component={this.renderInput} label="CHA" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="fields">
-                    <div className="two wide field">
+            <Form className="ui form" onSubmit={handleSubmit(this.onSubmit)}>
+                <Grid columns='equal'>
+                    <Grid.Column>
+                        <Field name="name" component={this.renderInput} label="Character Name"/>
                         <Field name="race_id" component={this.renderSelector} label="Race" selector={races} />
-                    </div>
-                    <div className="two wide field">
                         <Field name="class_id" component={this.renderSelector} label="Class" selector={classes} />
-                    </div>
-                    <div className="two wide field">
                         <Field name="alignment_id" component={this.renderSelector} label="Alignment" selector={alignments} />
-                    </div>
-                </div>
-                <button className="ui button primary" disabled={pristine}>Save</button>
-                <button className="ui button" onClick={reset} disabled={pristine}>Reset</button>
-            </form>
+                    </Grid.Column>
+                    <Grid columns={2} divided>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Field name="str" component={this.renderInput} label="STR" />
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Field name='int' component={this.renderInput} label='INT' />
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Field name="dex" component={this.renderInput} label="DEX" />
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Field name='wis' component={this.renderInput} label='WIS' />
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Field name="con" component={this.renderInput} label="CON" />
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Field name='cha' component={this.renderInput} label='cha' />
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Grid>
+                <Field name='retired' id='retired' component={this.renderCheckbox} label='Retired' />
+                <Grid centered>
+                    <Button className="positive" animated='vertical' disabled={pristine}>
+                        <Button.Content visible>Save</Button.Content>
+                        <Button.Content hidden>
+                            <Icon name='save' />
+                        </Button.Content>
+                    </Button>
+                    <Button animated disabled={pristine} onClick={reset}>
+                        <Button.Content visible>Reset</Button.Content>
+                        <Button.Content hidden>
+                            <Icon name='undo' />
+                        </Button.Content>
+                    </Button>
+                </Grid>
+            </Form>
         )
     }
 }
