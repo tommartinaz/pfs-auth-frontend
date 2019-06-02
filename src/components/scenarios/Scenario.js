15086e3bcf1a2c9d35b83a3c06dcf0d1d45b860c
Tom Martin <tom@Poe.local>
2019-06-02 05:35:02 -0700

    adds delete functionality to page

--- src/components/scenarios/Scenario.js
+++ src/components/scenarios/Scenario.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../redux/actions/scenarioActions';
+import { Button, Modal } from 'semantic-ui-react';
 
 class Scenario extends Component {
+    state = {
+        modalOpen: false
+    }
+
     componentDidMount() {
         this.props.fetchScenario(this.props.match.params.scenarioId);
     }
 
+    handleOpen = () => {
+        this.setState({ modalOpen: true });
+    }
+
+    handleClose = () => {
+        this.setState({ modalOpen: false });
+    }
+
+    deleteScenario = () => {
+        this.props.deleteScenario(this.props.match.params.scenarioId);
+        this.props.history.push('/scenarios');
+    }
+
     render() {
-        return !this.props.scenario ? <div>Loading...</div> :
-            <div>{this.props.scenario.name}</div>
+        const { scenario } = this.props;
+        if(!scenario) {
+            return <div>Loading...</div>
+        }
+        return (
+            <>
+                <Modal
+                    trigger={<Button onClick={this.handleOpen}>Delete</Button>}
+                    open={this.state.modalOpen}
+                    onClose={this.handleClose}
+                    size='tiny'
+                >
+                    <Modal.Header>Delete {scenario.name}</Modal.Header>
+                    <Modal.Description>
+                        Are you sure you want to delete {scenario.name}?
+                    </Modal.Description>
+                    <Modal.Actions>
+                        <Button color='red' onClick={this.deleteScenario}>Yes</Button>
+                        <Button color='green' onClick={this.handleClose}>No</Button>
+                    </Modal.Actions>
+                </Modal>
+            </>
+        )
     }
 }
 
