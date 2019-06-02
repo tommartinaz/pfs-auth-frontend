4f62585016ddc3c0497779a408e8771e6cee0fac
Tom Martin <tom@Poe.local>
2019-06-02 05:33:50 -0700

    adds styling to authentication pieces

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,42 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
+import { Button, Form, Grid, Segment, Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import { compose } from 'redux';
-import { signup } from '../../redux/actions/authActions';
+import * as actions from '../../redux/actions/authActions';
 
 class Signup extends Component {
 
     onSubmit = formValues => {
+        console.log(formValues);
         this.props.signup(formValues, () => {
             this.props.history.push('/characters');
         });
     }
 
+    renderInputField = ({ label, input, icon }) => {
+        return (
+            <Form.Input
+                icon={icon}
+                iconPosition='left'
+                placeholder={label}
+                {...input}
+            />
+        );
+    }
+
     render() {
         const { handleSubmit } = this.props;
         return (
-            <form onSubmit={handleSubmit(this.onSubmit)}>
-                <fieldset>
-                    <label>Email</label>
-                    <Field
-                        name="email"
-                        type="text"
-                        component="input"
-                        autoComplete="none"
-                    />
-                </fieldset>
-                <fieldset>
-                    <label>Password</label>
-                    <Field 
-                        name="password"
-                        type="password"
-                        component="input"
-                        autoComplete="none"
-                    />
-                </fieldset>
-                <div>{this.props.errorMessage}</div>
-                <button>Sign Up</button>
-            </form>
+            <Grid textAlign='center' verticalAlign='middle'>
+                <Grid.Column style={{ maxWidth: 450 }}>
+                    <Header as='h2' color='teal' textAlign='center'>
+                        Create an account
+                    </Header>
+                    <Form onSubmit={handleSubmit(this.onSubmit)}>
+                        <Segment>
+                            <Field name='name' component={this.renderInputField} label='Name' icon='user' />
+                            <Field name="email" component={this.renderInputField} label="E-mail address" icon='user' />
+                            <Field name='password' component={this.renderInputField} label='Password' icon='lock' />
+                            <Button color='teal' fluid size='large'>
+                                Create my account
+                            </Button>
+                        </Segment>
+                    </Form>
+                </Grid.Column>
+            </Grid>
         )
     }
 }
@@ -46,6 +55,6 @@ const mapStateToProps = state => ({
 });
 
 export default compose(
-    connect(mapStateToProps, { signup }),
+    connect(mapStateToProps, actions),
     reduxForm({ form: 'signup' })
 )(Signup);
